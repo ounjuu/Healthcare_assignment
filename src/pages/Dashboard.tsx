db27ae08c50c60f0577434a05cf8561765c56147
ounjuu <koj1555@naver.com>
2025-10-31 17:09:43 +0900

    Feat: tailwind css 적용

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+// src/pages/Dashboard.tsx
+import React from "react";
+
+const Dashboard: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-500">대시보드 테스트</h1>
+    </div>
+  );
+};
+
+export default Dashboard;

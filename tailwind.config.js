c64e497132c1ce157256e047677ce88248fada72
ounjuu <koj1555@naver.com>
2025-10-31 16:57:09 +0900

    Feat: tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        reddieetext: "#af0012",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-50%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 20s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+};

31a7e567ddf587fd48806adf709b042f338c8681
ounjuu <koj1555@naver.com>
2025-11-01 13:07:35 +0900

    Feat: css

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,30 +4,38 @@ const DashboardTitle: React.FC = () => {
   const [activeTitle, setActiveTitle] = useState<"basic" | "detail">("basic");
 
   return (
-    <div className="relative min-h-screen bg-[#F8F8F8]">
+    <div className="flex w-[1800px] h-[4195.28px] absolute top-[93px] left-[89px] rounded-tr-[36px] rounded-br-[36px] rounded-bl-[36px]">
       {/* 기본결과 */}
       <h2
         onClick={() => setActiveTitle("basic")}
-        className={`absolute w-[376px] h-[116px] top-[165.45px] left-[215px]
-          text-[96px] leading-[1] font-bold tracking-normal font-inter
-          cursor-pointer
-          ${activeTitle === "basic" ? "text-black" : "text-[#6B6B6B]"}
-        `}
+        className={`w-[376px] h-[116px] top-[165.45px] left-[215px]
+    leading-[1] font-bold tracking-normal font-inter rounded-tr-[36px] rounded-tl-[36px]
+    cursor-pointer text-[96px] transition-colors duration-300 flex items-center justify-center
+    ${
+      activeTitle === "basic"
+        ? "text-black bg-white"
+        : "text-[#6B6B6B] bg-transparent"
+    }
+  `}
       >
         기본결과
       </h2>
 
       {/* 세부결과 */}
-      <h2
+      <div
         onClick={() => setActiveTitle("detail")}
-        className={`absolute w-[376px] h-[116px] top-[165.45px] left-[812px]
-          text-[96px] leading-[1] font-bold tracking-normal font-inter
-          cursor-pointer
-          ${activeTitle === "detail" ? "text-black" : "text-[#6B6B6B]"}
-        `}
+        className={`w-[376px] h-[116px] top-[165.45px] left-[215px]
+    leading-[1] font-bold tracking-normal font-inter rounded-tr-[36px] rounded-tl-[36px]
+    cursor-pointer text-[96px] transition-colors duration-300  flex items-center justify-center
+    ${
+      activeTitle === "basic"
+        ? "text-[#6B6B6B] bg-transparent"
+        : "text-black bg-white"
+    }
+  `}
       >
         세부결과
-      </h2>
+      </div>
     </div>
   );
 };

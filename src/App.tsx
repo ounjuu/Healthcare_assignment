1ff93ff4c7dcd88071b3d7ad15be7b2c9cd86344
ounjuu <koj1555@naver.com>
2025-11-01 14:45:17 +0900

    Feat: css 설정 수정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <div className="min-h-screen bg-bgLight">
+    <div className="min-h-screen w-full bg-bgLight flex flex-col">
       <Dashboard />
     </div>
   );
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -9,7 +9,7 @@ export default function DashboardPage() {
   const [activeTitle, setActiveTitle] = useState<"basic" | "detail">("basic");
 
   return (
-    <div className="flex max-w-[1800px] max-h-[4195.28px] absolute top-[93px] left-[89px] rounded-[36px] flex-col">
+    <div className="flex pt-[93px] pb-[93px] pl-[89px] pr-[89px] rounded-[36px] flex-col w-full h-full">
       {/* 탭 헤더 */}
       <DashboardTabs
         activeTitle={activeTitle}
@@ -19,11 +19,11 @@ export default function DashboardPage() {
       {/* 탭 내용 */}
       <div className="w-full bg-white rounded-b-[36px] p-8">
         {activeTitle === "basic" ? (
-          <div className="flex flex-col w-screen h-screen p-8">
+          <div className="flex flex-col p-8">
             <BasicResult />
           </div>
         ) : (
-          <div className="flex flex-col w-screen h-screen p-8">
+          <div className="flex flex-col p-8">
             <DetailResult />
           </div>
         )}
